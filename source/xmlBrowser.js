e9abadce9d8af2e47c64389109adf22f3d4ec042
catamphetamine <purecatamphetamine@gmail.com>
2019-03-27 15:40:43 +0300

    #21 Fix non-trimmed XML

--- source/xmlBrowser.js
+++ source/xmlBrowser.js
@@ -2,7 +2,9 @@
 // https://github.com/goto100/xpath/issues/85
 export default {
 	createDocument(content) {
-		return new DOMParser().parseFromString(content, 'text/xml')
+		// A weird bug: it won't parse XML unless it's trimmed.
+		// https://github.com/catamphetamine/read-excel-file/issues/21
+		return new DOMParser().parseFromString(content.trim(), 'text/xml')
 	},
 
 	select(doc, node, path, namespaces = {}) {
