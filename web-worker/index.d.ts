900e9ea9a3f053c9efb9414ea093cba6f7b45fee
catamphetamine <kuchumovn@gmail.com>
2021-11-11 20:19:56 +0300

    .

--- web-worker/index.d.ts
+++ web-worker/index.d.ts
@@ -1,1 +1,17 @@
-export * from '../index.d';
+import {
+	ParseWithSchemaOptions,
+	ParseWithMapOptions,
+	ParseWithoutSchemaOptions,
+	ParsedObjectsResult,
+	Row
+} from '../types.d';
+
+export function parseExcelDate(excelSerialDate: number) : typeof Date;
+
+type Input = File;
+
+export function readXlsxFile<T extends object>(input: Input, options: ParseWithSchemaOptions<T>) : Promise<ParsedObjectsResult<T>>;
+export function readXlsxFile<T extends object>(input: Input, options: ParseWithMapOptions) : Promise<ParsedObjectsResult<T>>;
+export function readXlsxFile(input: Input, options?: ParseWithoutSchemaOptions) : Promise<Row[]>;
+
+export default readXlsxFile;
